b46d58c190f97a7e0ca04b3d99d0e56d8213fd15
Jacob Fougères <fougeresjacob@gmail.com>
2026-01-20 16:10:00 -0500

    Update directory path for dandybot execution

--- launcher.sh
+++ launcher.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/jacob/Documents/dandybot
+cd home/slumby/dandybot
 sudo python slayerbot.py
 cd /
